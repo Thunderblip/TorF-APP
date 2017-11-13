d0274aa95c9ff0f58575aa9ab8b44017e6ebd533
Frederick Hickling <hicf1_16@uni.worc.ac.uk>
2017-11-13 15:23:45 +0000

    I made it
    we did it Reddit

---
+++ touchGestureCode/www/js/index.js
@@ -0,0 +1,46 @@
+<!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
+
+$(document).on("pagecreate","#pageone",function(){
+    
+  
+  	$('#tapholdtext').on("taphold",function(){
+    	$(this).hide();
+ 	});                       
+
+	$('#taptext').on("tap",function(){
+    	$(this).css('color', 'red');
+ 	}); 
+
+	$('#swipetext').on("swipeleft",function(){
+    	$(this).css('color', 'green');
+  	});   
+    
+    $('#swipetext').on("swiperight",function(){
+    	$(this).css('color', 'blue');
+  	});   
+    
+    $('#TorF').on("tap",function(){
+    	 
+        //some stuff
+        var val = randomNumber();
+        
+        $("#myText").text(val);
+        
+  	});  
+
+
+});
+
+function randomNumber() {
+    return !!Math.round(Math.random());
+} 
+
+if (val = false) {
+    
+    navigator.notification.beep(1);
+}
+
+if (val = true) {
+    
+    navigator.notification.beep(2);
+}
